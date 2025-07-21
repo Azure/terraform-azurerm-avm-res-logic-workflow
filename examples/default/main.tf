32ec17871249197b37cc609c8f476603bad76ebc
Bala Krishnamoorthy <bakrish@microsoft.com>
2025-07-21 18:00:44 +0530

    AzAPI,AzureRM changes

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.5"
+  required_version = ">= 1.9, < 2.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.74"
+      version = "~> 4.0, >= 4.8.0, >= 4.21.1, < 5.0.0"
     }
     random = {
       source  = "hashicorp/random"
